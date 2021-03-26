71ab99fb55dbc07029046ebacbf1b16dbf1a11f1
unknown <uyharold86@gmail.com>
2021-03-27 01:51:58 +0800

    added dependencies and new lines of code at index.js and config.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag'); 26.3K (gzipped:7.8K)
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('config.js');
+
+const typeDefs = gql`
+    type Query{
+        sayHi: String!
+    }
+`
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello World!'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log('MongoDB Connected');
+        return server.listen({port: 5000});
+    })
+
+server.listen({port:5000})
+    .then(res => {
+       console.log(`Server running at ${res.url}`)
+    })
