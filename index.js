c8e70f756a7c28070f1531285c501275ba2d7012
unknown <uyharold86@gmail.com>
2021-03-28 19:04:55 +0800

    bug fixes

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ mongoose
   .then((res) => {
     console.log(`Server running at ${res.url}`);
   });
-  .catch(err => {
+  /* .catch(err => {
     console.error(err) // this will catch any error that happpens
-  }) 
+  }); */
